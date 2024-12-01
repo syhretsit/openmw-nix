b5262e2cec2a4e26a9d020b8a1aabdb8fdb41911
Dmitry Shestakov <syhretsit@gmail.com>
2024-12-02 02:11:05 +0800

    fix: add hash

--- packages/openmw-dev/package.nix
+++ packages/openmw-dev/package.nix
@@ -90,7 +90,7 @@ stdenv.mkDerivation {
         # rev = "9234b5707f9df8a75e86f3a7c3e6de9da4fb8f33";
         rev = "2aa249e82a964e44e48e5704303824dde54fbf44";
         # hash = "sha256-P2vxRt4LP83H5yRt+JOv3U+WJJOHBqhmYil++VznhRg=";
-        hash = "";
+        hash = "sha256-ATjJdhfICmXrLyAQxPWbb4UEKYOC7gT9O6Mx76Uqu0I=";
     };
 
     nativeBuildInputs = [ cmake pkg-config wrapQtAppsHook ];
