8eddca32107d5d5604c42a1d6c6596ecc5081ecf
PopeRigby <poperigby@mailbox.org>
2024-12-26 23:55:57 -0800

    Update

--- packages/openmw-dev/package.nix
+++ packages/openmw-dev/package.nix
@@ -1,118 +1,134 @@
 {
-    SDL2,
-    boost,
-    bullet,
-    cmake,
-    fetchFromGitHub,
-    fetchFromGitLab,
-    ffmpeg,
-    inputs,
-    libxml2,
-    luajit,
-    lz4,
-    minizip,
-    openal,
-    openscenegraph,
-    pkg-config,
-    qt6,
-    readline,
-    recastnavigation,
-    stdenv,
-    unshield,
-    wrapQtAppsHook,
-    xorg,
-    yaml-cpp,
-    ...
+  SDL2,
+  boost,
+  bullet,
+  cmake,
+  fetchFromGitHub,
+  fetchFromGitLab,
+  ffmpeg,
+  inputs,
+  libxml2,
+  luajit,
+  lz4,
+  minizip,
+  openal,
+  openscenegraph,
+  pkg-config,
+  qt6,
+  readline,
+  recastnavigation,
+  stdenv,
+  unshield,
+  wrapQtAppsHook,
+  xorg,
+  yaml-cpp,
+  ...
 }:
 
 let
-    GL = "GLVND";
+  GL = "GLVND";
 
-    collada-dom = stdenv.mkDerivation {
-        pname = "collada-dom";
-        version = "master";
+  collada-dom = stdenv.mkDerivation {
+    pname = "collada-dom";
+    version = "master";
 
-        src = fetchFromGitHub {
-            owner = "rdiankov";
-            repo = "collada-dom";
-            rev = "c1e20b7d6ff806237030fe82f126cb86d661f063";
-            sha256 = "A1ne/D6S0shwCzb9spd1MoSt/238HWA8dvgd+DC9cXc=";
-        };
-
-        postInstall = ''
-            chmod +w -R $out
-            mv $out/include/*/* $out/include
-        '';
-
-        nativeBuildInputs = [ cmake ];
-
-        buildInputs = [
-            boost
-            libxml2
-            minizip
-            readline
-        ];
+    src = fetchFromGitHub {
+      owner = "rdiankov";
+      repo = "collada-dom";
+      rev = "c1e20b7d6ff806237030fe82f126cb86d661f063";
+      sha256 = "A1ne/D6S0shwCzb9spd1MoSt/238HWA8dvgd+DC9cXc=";
     };
 
-    osg' = (openscenegraph.override { colladaSupport = true; }).overrideDerivation (old: {
-        COLLADA_DIR = collada-dom;
-        cmakeFlags = (old.cmakeFlags or []) ++ [
-            "-Wno-dev"
-            "-DOpenGL_GL_PREFERENCE=${GL}"
-            "-DBUILD_OSG_PLUGINS_BY_DEFAULT=0"
-            "-DBUILD_OSG_DEPRECATED_SERIALIZERS=0"
-        ] ++ (map (e: "-DBUILD_OSG_PLUGIN_${e}=1") [ "BMP" "DAE" "DDS" "FREETYPE" "JPEG" "OSG" "PNG" "TGA" ]);
-    });
+    postInstall = ''
+      chmod +w -R $out
+      mv $out/include/*/* $out/include
+    '';
 
-    bullet' = bullet.overrideDerivation (old: {
-        cmakeFlags = (old.cmakeFlags or []) ++ [
-            "-Wno-dev"
-            "-DOpenGL_GL_PREFERENCE=${GL}"
-            "-DUSE_DOUBLE_PRECISION=ON"
-            "-DBULLET2_MULTITHREADING=ON"
-        ];
-    });
+    nativeBuildInputs = [ cmake ];
 
-    mygui' = inputs.mygui.legacyPackages.x86_64-linux.mygui.overrideDerivation (old: {
-        cmakeFlags = (old.cmakeFlags or []) ++ [
-            "-DMYGUI_DONT_USE_OBSOLETE=ON"
-        ];
-    });
+    buildInputs = [
+      boost
+      libxml2
+      minizip
+      readline
+    ];
+  };
+
+  osg' = (openscenegraph.override { colladaSupport = true; }).overrideDerivation (old: {
+    COLLADA_DIR = collada-dom;
+    cmakeFlags =
+      (old.cmakeFlags or [ ])
+      ++ [
+        "-Wno-dev"
+        "-DOpenGL_GL_PREFERENCE=${GL}"
+        "-DBUILD_OSG_PLUGINS_BY_DEFAULT=0"
+        "-DBUILD_OSG_DEPRECATED_SERIALIZERS=0"
+      ]
+      ++ (map (e: "-DBUILD_OSG_PLUGIN_${e}=1") [
+        "BMP"
+        "DAE"
+        "DDS"
+        "FREETYPE"
+        "JPEG"
+        "OSG"
+        "PNG"
+        "TGA"
+      ]);
+  });
+
+  bullet' = bullet.overrideDerivation (old: {
+    cmakeFlags = (old.cmakeFlags or [ ]) ++ [
+      "-Wno-dev"
+      "-DOpenGL_GL_PREFERENCE=${GL}"
+      "-DUSE_DOUBLE_PRECISION=ON"
+      "-DBULLET2_MULTITHREADING=ON"
+    ];
+  });
+
+  mygui' = inputs.mygui.legacyPackages.x86_64-linux.mygui.overrideDerivation (old: {
+    cmakeFlags = (old.cmakeFlags or [ ]) ++ [
+      "-DMYGUI_DONT_USE_OBSOLETE=ON"
+    ];
+  });
 
 in
 stdenv.mkDerivation {
-    pname = "openmw";
-    version = "0.49.0";
+  pname = "openmw";
+  version = "0.49.0";
 
-    src = fetchFromGitLab {
-        owner = "OpenMW";
-        repo = "openmw";
-        rev = "9234b5707f9df8a75e86f3a7c3e6de9da4fb8f33";
-        hash = "sha256-P2vxRt4LP83H5yRt+JOv3U+WJJOHBqhmYil++VznhRg=";
-    };
+  src = fetchFromGitLab {
+    owner = "OpenMW";
+    repo = "openmw";
+    rev = "63b8e636f391e80388a7d7a5077c16cf9847fd64";
+    hash = "sha256-G3GF9pWL5II/ipmIhFLjXNjMHP/v0vqPU2iq1aq5+zo=";
+  };
 
-    nativeBuildInputs = [ cmake pkg-config wrapQtAppsHook ];
+  nativeBuildInputs = [
+    cmake
+    pkg-config
+    wrapQtAppsHook
+  ];
 
-    buildInputs = [
-        SDL2
-        boost
-        bullet'
-        ffmpeg
-        luajit
-        lz4
-        mygui'
-        openal
-        osg'
-        qt6.full
-        qt6.qtbase
-        recastnavigation
-        unshield
-        xorg.libXt
-        yaml-cpp
-    ];
+  buildInputs = [
+    SDL2
+    boost
+    bullet'
+    ffmpeg
+    luajit
+    lz4
+    mygui'
+    openal
+    osg'
+    qt6.full
+    qt6.qtbase
+    recastnavigation
+    unshield
+    xorg.libXt
+    yaml-cpp
+  ];
 
-    cmakeFlags = [
-        "-DOpenGL_GL_PREFERENCE=${GL}"
-        "-DOPENMW_USE_SYSTEM_RECASTNAVIGATION=1"
-    ];
+  cmakeFlags = [
+    "-DOpenGL_GL_PREFERENCE=${GL}"
+    "-DOPENMW_USE_SYSTEM_RECASTNAVIGATION=1"
+  ];
 }
