73101d8d0a7e098da25a8a8994f000c7c7d8b9a6
gbtb <goodbetterthebeast3@gmail.com>
2025-01-11 22:24:01 +1000

    umo: 0.8.5 -> 0.8.9

--- packages/umo/default.nix
+++ packages/umo/default.nix
@@ -1,18 +1,20 @@
 { python3Packages, fetchFromGitLab, lib, pkgs }:
 python3Packages.buildPythonApplication rec {
   pname = "umo";
-  version = "0.8.5";
+  version = "0.8.9";
 
   src = fetchFromGitLab {
     owner = "modding-openmw";
     repo = "umo";
     rev = version;
-    sha256 = "sha256-tktk5wey+dVYCx2xrp7oYlvBIyvjSe2hcZ/H4qAH858=";
+    sha256 = "sha256-QQfxBM7Uv+sJWhVd/EyD33mMfsDThlRdwK0NpHia7MM=";
   };
 
-  patches = [ ./shebang.patch ]; # have to add shebang ourselves, so that nix wrapping magic does the trick
-  pyproject = false; #this project has only requirements.txt
+  pyproject = true;
 
+  build-system = with python3Packages; [
+    setuptools
+  ];
   dependencies = with python3Packages; [
     coloredlogs
     desktop-notifier
@@ -26,25 +28,18 @@ python3Packages.buildPythonApplication rec {
     pwinput
     websockets
     certifi
+    beautifulsoup4
   ];
 
-  # This package has a Makefile which is used to compile! python code with nuitka compiler.
-  # IMO, it's an overkill for a small utility, and it makes stuff harder to package. 
-  # Instead we use pure python, and just have to override 2 phases that uses Makefile by default.
-  buildPhase = ''
-    mkdir -p $out/usr/share/icons $out/bin
+  pythonRelaxDeps = true;
 
-    cp -r src/* $out/bin
-    cp icons/KeenderM.png $out/usr/share/icons
-
-  '';
-
-  installPhase = ''
-    install -D -m +rx $out/bin/umo.py $out/bin/umo
-  '';
-
-  checkPhase = ''
-    $out/bin/umo --help
+  nativeInstallCheckInputs = [ pkgs.versionCheckHook ];
+  versionCheckProgramArg = "--version";
+  doInstallCheck = true;
+  installCheckPhase = ''
+    # have to import hook manually, something is broken for automatic import
+    source ${pkgs.versionCheckHook}/nix-support/setup-hook
+    versionCheckHook
   '';
 
   meta = {
