b55b4d4e188615bab1355b4647a7c0f66649e0fa
PopeRigby <poperigby@mailbox.org>
2025-10-19 15:45:06 -0700

    Make openmw-dev an overlay of the upstream package

--- packages/openmw-dev/default.nix
+++ packages/openmw-dev/default.nix
@@ -1,165 +1,22 @@
 {
-  qt6Packages,
-  inputs,
+  lib,
+  pkgs,
   tweakedWaterShader ? true,
   ...
 }:
 
-qt6Packages.callPackage
-  (
-    {
-      SDL2,
-      boost,
-      bullet,
-      cmake,
-      fetchFromGitHub,
-      fetchFromGitLab,
-      fetchurl,
-      ffmpeg,
-      inputs,
-      libxml2,
-      luajit,
-      lz4,
-      minizip,
-      openal,
-      openscenegraph,
-      pkg-config,
-      qt6,
-      readline,
-      recastnavigation,
-      stdenv,
-      unshield,
-      wrapQtAppsHook,
-      xorg,
-      yaml-cpp,
-      lib,
+pkgs.openmw.overrideAttrs (old: {
+  pname = "openmw";
+  version = "openmw-0.49.0-unstable-2025-10-04";
 
-      tweakedWaterShader,
-      ...
-    }:
+  src = pkgs.fetchFromGitLab {
+    owner = "OpenMW";
+    repo = "openmw";
+    rev = "ef05e089abcb7f76bca4601f4acec4f40e438a62";
+    hash = "sha256-0vIASx/EkwCzLUXXo2VhOE2k+JPLtJ4A1d3bdUXT4rI=";
+  };
 
-    let
-      inherit (lib) optionals;
-
-      GL = "GLVND";
-
-      collada-dom = stdenv.mkDerivation {
-        pname = "collada-dom";
-        version = "master";
-
-        src = fetchFromGitHub {
-          owner = "rdiankov";
-          repo = "collada-dom";
-          rev = "c1e20b7d6ff806237030fe82f126cb86d661f063";
-          sha256 = "A1ne/D6S0shwCzb9spd1MoSt/238HWA8dvgd+DC9cXc=";
-        };
-
-        patches = [
-          (fetchurl {
-            url = "https://aur.archlinux.org/cgit/aur.git/plain/fix-compatibility-with-boost-1.85.patch?h=collada-dom";
-            hash = "sha256-z6sC8ASLRe6lkobR0FTbLl176upnXNKyITDKyVVxOVU=";
-          })
-        ];
-
-        postInstall = ''
-          chmod +w -R $out
-          mv $out/include/*/* $out/include
-        '';
-
-        nativeBuildInputs = [ cmake ];
-
-        buildInputs = [
-          boost
-          libxml2
-          minizip
-          readline
-        ];
-      };
-
-      osg' = (openscenegraph.override { colladaSupport = true; }).overrideDerivation (old: {
-        COLLADA_DIR = collada-dom;
-        cmakeFlags =
-          (old.cmakeFlags or [ ])
-          ++ [
-            "-Wno-dev"
-            "-DOpenGL_GL_PREFERENCE=${GL}"
-            "-DBUILD_OSG_PLUGINS_BY_DEFAULT=0"
-            "-DBUILD_OSG_DEPRECATED_SERIALIZERS=0"
-          ]
-          ++ (map (e: "-DBUILD_OSG_PLUGIN_${e}=1") [
-            "BMP"
-            "DAE"
-            "DDS"
-            "FREETYPE"
-            "JPEG"
-            "OSG"
-            "PNG"
-            "TGA"
-          ]);
-      });
-
-      bullet' = bullet.overrideDerivation (old: {
-        cmakeFlags = (old.cmakeFlags or [ ]) ++ [
-          "-Wno-dev"
-          "-DOpenGL_GL_PREFERENCE=${GL}"
-          "-DUSE_DOUBLE_PRECISION=ON"
-          "-DBULLET2_MULTITHREADING=ON"
-        ];
-      });
-
-      mygui' = inputs.mygui.legacyPackages.x86_64-linux.mygui.overrideDerivation (old: {
-        cmakeFlags = (old.cmakeFlags or [ ]) ++ [
-          "-DMYGUI_DONT_USE_OBSOLETE=ON"
-        ];
-      });
-
-    in
-    stdenv.mkDerivation {
-      pname = "openmw";
-      version = "openmw-0.49.0-unstable-2025-10-04";
-
-      src = fetchFromGitLab {
-        owner = "OpenMW";
-        repo = "openmw";
-        rev = "ef05e089abcb7f76bca4601f4acec4f40e438a62";
-        hash = "sha256-0vIASx/EkwCzLUXXo2VhOE2k+JPLtJ4A1d3bdUXT4rI=";
-      };
-
-      # Tweaks core water shader to work with Wareya's SSR post processing water shader
-      # https://github.com/wareya/OpenMW-Shaders?tab=readme-ov-file#using-the-water-shader
-      patches = optionals tweakedWaterShader [ ./tweaked_water_shader.patch ];
-
-      nativeBuildInputs = [
-        cmake
-        pkg-config
-        wrapQtAppsHook
-      ];
-
-      buildInputs = [
-        SDL2
-        boost
-        bullet'
-        ffmpeg
-        luajit
-        lz4
-        mygui'
-        openal
-        osg'
-        qt6.full
-        qt6.qtbase
-        recastnavigation
-        unshield
-        xorg.libXt
-        yaml-cpp
-      ];
-
-      cmakeFlags = [
-        "-DOpenGL_GL_PREFERENCE=${GL}"
-        "-DOPENMW_USE_SYSTEM_RECASTNAVIGATION=1"
-      ];
-    }
-  )
-  {
-    inputs = inputs;
-    tweakedWaterShader = tweakedWaterShader;
-  }
+  # Tweaks core water shader to work with Wareya's SSR post processing water shader
+  # https://github.com/wareya/OpenMW-Shaders?tab=readme-ov-file#using-the-water-shader
+  patches = lib.optionals tweakedWaterShader [ ./tweaked_water_shader.patch ];
+})
