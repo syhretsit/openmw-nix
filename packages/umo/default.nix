c5803466811f5d283eda4f3c2311f6615fb3fa54
Dmitry Shestakov <syhretsit@gmail.com>
2026-02-17 02:50:26 +0800

    fix: license-file umo in pyproject.toml

--- packages/umo/default.nix
+++ packages/umo/default.nix
@@ -55,6 +55,7 @@ python3Packages.buildPythonApplication rec {
 
   postPatch = ''
     sed -i 's|license = {text = "GNU AGPLv3"}|license = "GNU AGPLv3"|' pyproject.toml
+    sed -i '/license = "GNU AGPLv3"/a license-files = ["LICENSE"]' pyproject.toml
   '';
 
   meta = {
