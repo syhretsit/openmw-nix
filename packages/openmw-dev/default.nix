0f84c602a3729aec29d37be6de37394f04ff055d
Dmitry Shestakov <syhretsit@gmail.com>
2026-04-01 14:08:18 +0800

    feat: update to branch 'reduce_dyn_casts_lua

--- packages/openmw-dev/default.nix
+++ packages/openmw-dev/default.nix
@@ -12,8 +12,8 @@ pkgs.openmw.overrideAttrs (old: {
   src = pkgs.fetchFromGitLab {
     owner = "OpenMW";
     repo = "openmw";
-    rev = "ebcdbb152e5a5e2380e5e177bbe25abb344394a9";
-    hash = "sha256-96GJDfWszXXwl9Dfc1Ka5V6148EYX0V+acRgkbSO8i0=";
+    rev = "757d791fe7ccdecea1aa03bdd6e5e4868b2780a5";
+    hash = "";
   };
 
   # Tweaks core water shader to work with Wareya's SSR post processing water shader
